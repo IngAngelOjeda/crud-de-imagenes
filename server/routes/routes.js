fa5473915f9272047952ac339d973379ccbe66a0
mundanox <bloodsekertom@gmail.com>
2022-05-13 18:23:22 -0400

    add files

---
+++ server/routes/routes.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+
+const router = express.Router()
+
+const diskstorage = multer.diskStorage({
+    destination: path.join(__dirname, '../images'),
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-archivo' + file.originalname)
+    }
+})
+
+const fileUpload = multer({
+    storage: diskstorage
+}).single('image')
+
+router.get('/', (req, res) => {
+    res.send('Welcome to my image app')
+})
+
+router.post('/images/post', fileUpload,(req, res) => {
+
+    req.getConnection((err, conn) => {
+        if(err) return res.status(500).send('server error')
+
+        const type = req.file.mimetype
+        const name = req.file.originalname
+        const data = fs.readFileSync(path.join(__dirname, '../images/' + req.file.filename))
+
+        conn.query('INSERT INTO image set ?', [{type, name, data}], (err, rows) => {
+            if(err) return res.status(500).send('server error')
+
+            res.send('image saved!')
+        })
+    })
+    
+})
+
+router.get('/images/get',(req, res) => {
+
+    req.getConnection((err, conn) => {
+        if(err) return res.status(500).send('server error')
+
+        conn.query("SELECT * FROM image ", (err, rows) => {
+            if(err) return res.status(500).send('server error')
+
+            rows.map(img => {
+            
+                fs.writeFileSync(path.join(__dirname, "../dbimages/" + img.id + "-archivo.png"), img.data)
+
+            })
+
+            const imagedir = fs.readdirSync(path.join(__dirname, "../dbimages/"))
+
+            res.json(imagedir)
+        })
+    })
+    
+})
+
+router.delete('/images/delete:id',(req, res) => {
+
+    req.getConnection((err, conn) => {
+        if(err) return res.status(500).send('server error')
+
+        conn.query("DELETE * FROM image WHERE id = ?",[req.params.id], (err, rows) => {
+            if(err) return res.status(500).send('server error')
+            
+            fs.unlinkSync(path.join(__dirname, "../dbimages/" + req.params.id + "-archivo.png"))
+
+            res.send("Image deleted")
+        })
+    })
+    
+})
+
+module.exports = router
